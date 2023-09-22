49b92c658893356769f914dd21fa36ee1d27becf
Luis Romo Portillo <luisrp9812@gmail.com>
2023-09-21 23:45:34 -0700

    Added: Navbar menu and his animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,23 @@
 let navbar_button = document.querySelector('.navbar-button')
 navbar_button.addEventListener('click', () => {
+    // Get the elements
     let icon_bars = document.querySelector('.fa-bars')
     let icon_xmark = document.querySelector('.fa-xmark')
+    let navbar_menu = document.querySelector('.navbar-menu')
 
+    // Toggle the classes
+    // If the navbar is closed, open it and change the icon from bars to xmark
     if (!icon_bars.classList.contains('bars-animated')) {
         icon_bars.classList.add('bars-animated')
         icon_xmark.classList.add('xmark-animated')
-    } else {
+        navbar_menu.classList.remove('navbar-menu-close')
+        navbar_menu.classList.add('navbar-menu-open')
+    }
+    // If the navbar is open, close it and change the icon from xmark to bars
+    else {
         icon_bars.classList.remove('bars-animated')
         icon_xmark.classList.remove('xmark-animated')
+        navbar_menu.classList.remove('navbar-menu-open')
+        navbar_menu.classList.add('navbar-menu-close')
     }
 })
-
-// let navbar_button = document.querySelector('.navbar-button')
-// navbar_button.addEventListener('click', () => {
-//     let icon_bars = document.querySelector('.fa-bars')
-//     let icon_xmark = document.querySelector('.fa-xmark')
-
-//     if (!icon_bars.classList.contains('icon-active')) {
-//         icon_bars.classList.add('icon-active')
-//         icon_xmark.classList.add('icon-deactive')
-//     } else {
-//         icon_bars.classList.remove('icon-active')
-//         icon_bars.classList.add('icon-deactive')
-
-//         icon_xmark.classList.remove('icon-deactive')
-//         icon_xmark.classList.add('icon-active')
-//     }
-
-// })
-
-// let icon_active = document.querySelector('.icon-active')
-// let icon_deactive = document.querySelector('.icon-deactive')
-// icon_active.classList.remove('icon-active')
-// icon_active.classList.add('icon-deactive')
-// icon_deactive.classList.remove('icon-deactive')
-// icon_deactive.classList.add('icon-active')
